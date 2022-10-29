fde9b6f29b8a39f4b81213a2e94ee43001cda0fd
Connor Dillon <connor@codewithconnor.com>
2022-10-28 18:27:03 -0700

    :recycle: Refactored HTML scrubbing and injection for ease of use moving forward

--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 window.addEventListener(
   'load',
   (e) => {
+    // Add link to style.css
     var link = document.createElement('link')
     link.rel = 'stylesheet'
     link.type = 'text/css'
     link.href = './style.css'
     document.head.appendChild(link)
+
+    // Logic for replacing website body with our app
+    var body = document.querySelector('body')
+    var main = document.createElement('main')
+    body.innerHTML = ''
+    document.body.appendChild(main)
+    main.innerHTML = `
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-primary"><a href="http://"></a>Link</div>
+    <div class="btn btn-wide"><a href="http://"></a>Continue</div>`
   },
   false
 )
-
-// Selectors
-const body = document.querySelector('body')
-const page = body.parentNode
-
-// HTML Injectors
-body.innerHTML = `<main class="container">
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-primary"><a href="http://"></a>Link</div>
-<div class="btn btn-wide"><a href="http://"></a>Continue</div>
-</main>`
